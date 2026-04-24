18e242d2f4859811b82aa8341112e0f1453730a7
ujin2022 <ujin2022@naver.com>
2026-04-24 12:06:40 +0900

    style: format_leaderboard_entry 주석처리

--- coding-everyday/solutions/default_parameters_practice.py
+++ coding-everyday/solutions/default_parameters_practice.py
@@ -7,5 +7,4 @@ def calculate_shipping(price, discount_rate=0.0, shipping_fee=3000):
     total = price * (1 - (discount_rate/100)) + shipping_fee
     return f"{discount_rate} 할인, 배송비 {shipping_fee}"
 
-def format_leaderboard_entry(rank, name, score, label):
-    
+#def format_leaderboard_entry(rank, name, score, label):
